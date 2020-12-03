a355aece5bb092e71a693c640855ee4475c9fb21
Tom Keepings <tom.keepings@prodigi.uk>
2020-12-03 13:26:17 +0000

    initial

---
+++ AoC-3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var numOfLines = lines.Count();
+            var lineLength = lines[0].Length;
+            var map2d = new char[numOfLines, lineLength]; // give space to copy paste loads of blocks in
+
+            // Populate 2d array
+            for (var i = 0; i < numOfLines; i++)
+            {
+                var thisline = lines[i];
+                var thislinearray = thisline.ToCharArray();
+
+                for (var j = 0; j < lineLength; j++)
+                {
+                    map2d[i, j] = thislinearray[j]; 
+                }
+            }
+
+            var excercise1 = CountTrees(map2d, numOfLines, lineLength, 1, 1);
+            var excercise2 = CountTrees(map2d, numOfLines, lineLength, 1, 3);
+            var excercise3 = CountTrees(map2d, numOfLines, lineLength, 1, 5);
+            var excercise4 = CountTrees(map2d, numOfLines, lineLength, 1, 7);
+            var excercise5 = CountTrees(map2d, numOfLines, lineLength, 2, 1);
+        }
+
+        private static int CountTrees(char[,] map, int totalLines, int lineLength, int xIncrement, int yIncrement)
+        {
+            var treeCounter = 0;
+            var y = 0;
+            for (var x = 0; x < totalLines; x+=xIncrement)
+            {
+                if (map[x, y] == '#')
+                {
+                    treeCounter++;
+                }
+
+                y += yIncrement;
+
+                if (y >= lineLength)
+                {
+                    // reset y to start of line because whole pattern repeats
+                    y -= lineLength;
+                }
+            }
+
+            Console.WriteLine(treeCounter);
+            return treeCounter;
+        }
+    }
+}
